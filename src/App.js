1e32e8afed6e751e93c409358cc3676b85016ed8
Damian Demasi <damian.demasi.1@gmail.com>
2022-01-12 08:29:06 +1030

    Initial commit: React and Bootstrap instalation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert';
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                Web Dev Progress Tracker
+                <Alert variant='success'>Bootstrap up and running!</Alert>
+            </header>
+        </div>
+    );
+}
+
+export default App;

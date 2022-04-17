0214dd5dec2b707e1d0afacd9ec8a2294d6a93b9
Damian Demasi <damian@amplify-now.com>
2022-04-17 19:13:25 +0930

    Modal added.

--- src/components/AddTopic.js
+++ src/components/AddTopic.js
@@ -1,16 +1,69 @@
-import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { Card, Col, Row } from 'react-bootstrap';
+import { Button, Modal, Card, Col, Row, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 function AddTopic(props) {
+    const [state, setState] = useState({
+        show: false,
+    });
+
+    console.log(state.show);
+
+    const addTopicModal = (
+        <Modal show={state.show} onHide={() => {}}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add a new topic</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Text className='text-muted'>
+                        Woohoo, you're adding a new topic!
+                    </Form.Text>
+                    <Form.Group className='mb-3' controlId='formBasicText'>
+                        {/* <Form.Label>Topic name</Form.Label> */}
+                        <Form.Control type='text' placeholder='Topic name' />
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='formBasicDescription'>
+                        {/* <Form.Label>Description</Form.Label> */}
+                        <Form.Control type='text' placeholder='Topic description`' />
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+                        <Form.Check type='checkbox' label='Check me out' />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant='secondary'
+                    onClick={() =>
+                        setState({
+                            show: false,
+                        })
+                    }>
+                    Close
+                </Button>
+                <Button variant='primary' onClick={() => {}}>
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+
     return (
         <>
+            {addTopicModal}
             <Card className='m-5 p-3 bg-light'>
                 <Row className=''>
                     <Col sm={2}>{/* <User /> */}</Col>
                     <Col>
-                        <Button variant='primary'>
+                        <Button
+                            variant='primary'
+                            onClick={() =>
+                                setState({
+                                    show: true,
+                                })
+                            }>
                             <FontAwesomeIcon
                                 icon={faPlus}
                                 style={{ marginRight: '10px' }}
